084a93404edc81cf8c250f3f810fe34a5e9ed2a3
Naut van der Winden <supposedbrit@gmail.com>
2024-11-05 11:32:27 +0100

    .

--- sources/js/s3.js
+++ sources/js/s3.js
@@ -1,4 +1,4 @@
-s3 = Array.from(document.getElementById("section-3").children).splice(1);
+s3 = Array.from(document.getElementById("section-journey").children).splice(1);
 
 function hover(element){
     s3.forEach(element => {
